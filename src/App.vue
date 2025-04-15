b0d8becce23bd7be75ac2a231c5d8d9132e87b02
Generalweeaboo1 <robertscharles503@gmail.com>
2025-04-15 03:35:46 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -109,25 +109,25 @@ export default {
           "mech": "Lionheart"
         },
         {
-          "callsign": "Kal",
-          "alias": "Vier Flau",
+          "callsign": "Nobody",
+          "alias": "Montague Estiechanteau",
           "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Necrophile"
+          "mech": "Holy Moly"
         },
         {
-          "callsign": "Starboard",
-          "alias": "Maria",
+          "callsign": "Manticore",
+          "alias": "Ioulietta Estiechanteau",
           "code": "4be26ce9-923b-4069-b6c9-76437d4be455///NDL-C-DEEP-STATION//056940c6-8d55-4190-8e85-57caa043cb1a",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Scarlet Judicator"
+          "mech": "Cyberpositive"
         },
        
         {
-          "callsign": "Compadre",
-          "alias": 'Clint "CC" Cruz',
+          "callsign": "Vivienne Castielle",
+          "alias": "Ishtar",
           "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
           "corpro": "GMS",
           "frame": "Everest",
