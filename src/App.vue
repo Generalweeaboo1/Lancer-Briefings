2dfd7757a7510723de3abf311ec50e81421935bb
Generalweeaboo1 <robertscharles503@gmail.com>
2025-04-15 03:24:31 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -124,14 +124,7 @@ export default {
           "frame": "Everest",
           "mech": "Scarlet Judicator"
         },
-        {
-          "callsign": "Badlands",
-          "alias": "Marlene Gatsby",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Mayfly"
-        },
+       
         {
           "callsign": "Compadre",
           "alias": 'Clint "CC" Cruz',
